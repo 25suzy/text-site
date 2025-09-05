a9b73728dfc8d7ac024830d8f4c1d97ec48bd902
suzy <sujiq0711@gmail.com>
2025-09-05 09:42:54 +0900

    Update Reveal.jsx

--- components/Reveal.jsx
+++ components/Reveal.jsx
@@ -18,8 +18,8 @@ export default function Reveal({ children, delay = 0, className = "" }) {
   return (
     <div
       ref={ref}
-      style={{ transitionDelay: `${delay}ms` }}
-      className={`transition-all duration-[var(--reveal-duration)] ease-out will-change-transform
+      style={{ transitionDelay: `${delay}ms`, transitionDuration: "var(--reveal-duration)" }}
+      className={`transition-all ease-out will-change-transform
         ${on ? "opacity-100 translate-y-0" : "opacity-0 translate-y-3"} ${className}`}
     >
       {children}
